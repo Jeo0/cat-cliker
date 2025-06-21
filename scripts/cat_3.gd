8f200ad051bec0374d1fc711cc71f99e8926999b
Jeo0 <jjrelb@gmail.com>
2025-06-22 02:51:15 +0800

    init

---
+++ scripts/cat_3.gd
@@ -0,0 +1,28 @@
+extends Sprite2D
+
+
+@onready var sfx_death: AudioStreamPlayer2D = $"../sfx_death"
+@onready var timer: Timer = $"../Timer"
+@onready var animated_sprite_2d: AnimatedSprite2D = $"../AnimatedSprite2D"
+
+func _ready():
+	animated_sprite_2d.hide();
+	self.show()
+
+func _input(event):
+	if event.is_action_pressed("click"):
+		if is_pixel_opaque(get_local_mouse_position()):
+			print("Cat3 clicked: Whumaba");
+
+			sfx_death.play();
+
+			# play death animation
+			self.hide()
+			animated_sprite_2d.show()
+			animated_sprite_2d.frame = 0
+			animated_sprite_2d.play("death") 
+			timer.start()
+
+func _on_timer_timeout():
+	animated_sprite_2d.queue_free()
+	self.queue_free()
---
+++ scripts/level1_bg.gd
@@ -0,0 +1,11 @@
+extends Sprite2D
+
+@onready var bg_sound = $"bg_sound"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	bg_sound.play()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
